17bd70639e81193d65933b58e3b38c77fd673911
Parul Singh <parsingh@redhat.com>
2021-02-11 09:15:10 -0500

    change source sync condition
    Signed-off-by: Parul Singh <parsingh@redhat.com>

--- test-kuttl/e2e/simple-rclone/08-waitfor-sync-source.sh
+++ test-kuttl/e2e/simple-rclone/08-waitfor-sync-source.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail
 
-while [[ $(kubectl -n "$NAMESPACE" get ReplicationSource/source -o jsonpath="{.status.conditions[0].lastTransitionTime}}") == "<no value>" ]]; do
+while [[ $(kubectl -n "$NAMESPACE" get ReplicationSource/source -o jsonpath="{.status.lastSyncTime}") == "<no value>" ]]; do
     sleep 1
 done
 
