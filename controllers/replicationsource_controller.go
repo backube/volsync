6ad2423cf232cb3b816fab1a613df89192f1880a
Ryan Cook <rcook@redhat.com>
2021-03-18 15:22:50 -0400

    port definition
    Signed-off-by: Ryan Cook <rcook@redhat.com>

--- controllers/replicationsource_controller.go
+++ controllers/replicationsource_controller.go
@@ -21,6 +21,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -537,11 +538,17 @@ func (r *rsyncSrcReconciler) ensureJob(l logr.Logger) (bool, error) {
 			r.job.Spec.Template.Spec.Containers = []corev1.Container{{}}
 		}
 		r.job.Spec.Template.Spec.Containers[0].Name = "rsync"
-		if r.Instance.Spec.Rsync.Address != nil {
+		if r.Instance.Spec.Rsync.Port != nil {
+			connectPort := strconv.Itoa(int(*r.Instance.Spec.Rsync.Port))
+			r.job.Spec.Template.Spec.Containers[0].Env = []corev1.EnvVar{
+				{Name: "DESTINATION_ADDRESS", Value: *r.Instance.Spec.Rsync.Address},
+				{Name: "DESTINATION_PORT", Value: connectPort},
+			}
+		} else if r.Instance.Spec.Rsync.Port == nil {
 			r.job.Spec.Template.Spec.Containers[0].Env = []corev1.EnvVar{
 				{Name: "DESTINATION_ADDRESS", Value: *r.Instance.Spec.Rsync.Address},
 			}
-		} else {
+		} else if r.Instance.Spec.Rsync.Address == nil {
 			r.job.Spec.Template.Spec.Containers[0].Env = []corev1.EnvVar{}
 		}
 		r.job.Spec.Template.Spec.Containers[0].Command = []string{"/bin/bash", "-c", "/source.sh"}
