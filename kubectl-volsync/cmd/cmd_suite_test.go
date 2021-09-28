e86ee0adac47d26984967ef30f08818154ae43ed
John Strunk <jstrunk@redhat.com>
2021-09-28 14:25:41 -0400

    Add testenv boilerplate to kubectl-volsync suite
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- kubectl-volsync/cmd/cmd_suite_test.go
+++ kubectl-volsync/cmd/cmd_suite_test.go
@@ -1,13 +1,65 @@
 package cmd
 
 import (
+	"path/filepath"
 	"testing"
 
+	snapv1 "github.com/kubernetes-csi/external-snapshotter/client/v4/apis/volumesnapshot/v1beta1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"k8s.io/client-go/rest"
+	"k8s.io/kubectl/pkg/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	volsyncv1alpha1 "github.com/backube/volsync/api/v1alpha1"
 )
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
 
 func TestCmd(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "kubectl-volsync Suite")
+	RunSpecsWithDefaultAndCustomReporters(t, "kubectl-volsync Suite", []Reporter{printer.NewlineReporter{}})
 }
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.UseDevMode(true), zap.WriteTo(GinkgoWriter)))
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			// VolSync CRDs
+			filepath.Join("..", "..", "config", "crd", "bases"),
+			// Snapshot CRDs
+			filepath.Join("..", "..", "hack", "crds"),
+		},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+	cfg, err = testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(cfg).ToNot(BeNil())
+
+	err = volsyncv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = snapv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	// From original boilerplate
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
+}, 60)
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).ToNot(HaveOccurred())
+})
