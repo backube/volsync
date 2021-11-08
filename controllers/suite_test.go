0b2e0e7cf42f3374373348761643c54efc35dd15
John Strunk <jstrunk@redhat.com>
2021-11-08 13:17:25 -0500

    Remove unused var
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -29,7 +29,6 @@ import (
 	gomegatypes "github.com/onsi/gomega/types"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
@@ -50,7 +49,6 @@ const (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
 
