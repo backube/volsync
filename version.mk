4ba995a53b9d06b28e120a40755bb711cd1e53fa
Tesshu Flower <tflower@redhat.com>
2022-03-18 14:35:33 -0400

    Fix version info in mover builds, use common version.mk file
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

---
+++ version.mk
@@ -0,0 +1,18 @@
+# Set versions to be used in the build here
+
+# VERSION defines the project version for the bundle.
+# Update this value when you upgrade the version of your project.
+# To re-generate a bundle for another specific version without changing the standard setup, you can:
+# - use the VERSION as arg of the bundle target (e.g make bundle VERSION=0.0.2)
+# - use environment variables to overwrite this value (e.g export VERSION=0.0.2)
+
+# Bundle Version being built right now and channels to use
+VERSION := 0.4.0
+CHANNELS := stable,acm-2.5
+DEFAULT_CHANNEL := stable
+
+HEAD_COMMIT ?= $(shell git rev-parse --short HEAD)
+DIRTY ?= $(shell git diff --quiet || echo '-dirty')
+BUILD_VERSION := v$(VERSION)+$(HEAD_COMMIT)$(DIRTY)
+
+BUILDDATE := $(shell date -u '+%Y-%m-%dT%H:%M:%S.%NZ')
