347c4f89d4ee7d516b4121bb8d41c6777cb4d4c6
John Strunk <jstrunk@redhat.com>
2022-04-19 09:48:06 -0400

    Fix type assertion
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- controllers/mover/restic/restic_test.go
+++ controllers/mover/restic/restic_test.go
@@ -169,7 +169,7 @@ var _ = Describe("Restic properly registers", func() {
 		It("is added to the mover catalog", func() {
 			found := false
 			for _, v := range mover.Catalog {
-				if v != nil {
+				if _, ok := v.(*Builder); ok {
 					found = true
 				}
 			}
