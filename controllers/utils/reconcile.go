04487c791481d124a8f87bf9a37967145cb4929b
Tesshu Flower <tflower@redhat.com>
2022-07-04 15:13:30 -0400

    Delete/recreate jobs if immutable field needs update
    For issue https://github.com/backube/volsync/issues/291
    
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

--- controllers/utils/reconcile.go
+++ controllers/utils/reconcile.go
@@ -17,7 +17,17 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 package utils
 
-import "github.com/go-logr/logr"
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/go-logr/logr"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
 
 // reconcileFunc is a function that partially reconciles an object. It returns a
 // bool indicating whether reconciling should continue and an error.
@@ -32,4 +42,27 @@ func ReconcileBatch(l logr.Logger, reconcileFuncs ...ReconcileFunc) (bool, error
 		}
 	}
 	return true, nil
+}
+
+// If an update causes an immutable error, delete the object and return an error (or potentially
+// an error from the delete if the delete fails).
+// The caller should ensure (usually via a requeue) that createOrUpdate is called on the resource again in
+// order for it to be recreated.
+func CreateOrUpdateDeleteOnImmutableErr(ctx context.Context, k8sClient client.Client, obj client.Object,
+	log logr.Logger, f ctrlutil.MutateFn) (ctrlutil.OperationResult, error) {
+	op, err := ctrlutil.CreateOrUpdate(ctx, k8sClient, obj, f)
+
+	// Check if we got an error trying to update an immutable field
+	if err != nil && kerrors.IsInvalid(err) && strings.Contains(strings.ToLower(err.Error()), "field is immutable") {
+		log.Error(err, "Immutable error updating the object. Will delete so it can be recreated")
+
+		delErr := k8sClient.Delete(ctx, obj, client.PropagationPolicy(metav1.DeletePropagationBackground))
+		if delErr != nil {
+			return op, delErr
+		}
+
+		return op, fmt.Errorf("unable to update object. Deleting object so it can be recreated")
+	}
+
+	return op, err
 }
