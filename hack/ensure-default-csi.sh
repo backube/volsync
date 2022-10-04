127a10bf91e70628b8060cbae02b4f9cc259eac4
Tesshu Flower <tflower@redhat.com>
2022-10-04 16:21:52 -0400

    shellcheck suggested fixes in .sh script
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

--- hack/ensure-default-csi.sh
+++ hack/ensure-default-csi.sh
@@ -6,32 +6,32 @@ set -e -o pipefail
 
 DEFAULT_STORAGE_CLASS=$(kubectl get storageclasses -o=jsonpath='{range .items[?(.metadata.annotations.storageclass\.kubernetes\.io/is-default-class=="true")]}{.metadata.name}{" "}{.provisioner}')
 
-DEFAULT_STORAGE_CLASS_NAME=$(echo ${DEFAULT_STORAGE_CLASS} | awk '{print $1}')
-DEFAULT_PROVISIONER=$(echo ${DEFAULT_STORAGE_CLASS} | awk '{print $2}')
+DEFAULT_STORAGE_CLASS_NAME=$(echo "${DEFAULT_STORAGE_CLASS}" | awk '{print $1}')
+DEFAULT_PROVISIONER=$(echo "${DEFAULT_STORAGE_CLASS}" | awk '{print $2}')
 
-if [[ ! -z "$DEFAULT_STORAGE_CLASS_NAME" ]]; then
+if [[ -n "${DEFAULT_STORAGE_CLASS_NAME}" ]]; then
   # Found a default storage class
-  if [[ "$DEFAULT_PROVISIONER" =~ .*"csi".* ]]; then
-    echo "Default storage class $DEFAULT_STORAGE_CLASS_NAME is csi, no updates required."
+  if [[ "${DEFAULT_PROVISIONER}" =~ .*"csi".* ]]; then
+    echo "Default storage class ${DEFAULT_STORAGE_CLASS_NAME} is csi, no updates required."
     exit 0
   fi
 
   # We need to remove the default annotation
-  kubectl annotate sc/${DEFAULT_STORAGE_CLASS_NAME} storageclass.kubernetes.io/is-default-class-
+  kubectl annotate sc/"${DEFAULT_STORAGE_CLASS_NAME}" storageclass.kubernetes.io/is-default-class-
 fi
 
 # No default set at this point, try to find a csi driver to use as default
 STORAGE_CLASSES=$(kubectl get storageclasses -o=jsonpath='{range .items[*]}{@.metadata.name}{"\t"}{@.provisioner}{"\n"}{end}')
 
 IFS=$'\n'
-for sc in $STORAGE_CLASSES; do
-  echo "sc is $sc"
-  SC_NAME=$(echo ${sc} | awk '{print $1}')
-  PROVISIONER=$(echo ${sc} | awk '{print $2}')
+for sc in ${STORAGE_CLASSES}; do
+  echo "sc is ${sc}"
+  SC_NAME=$(echo "${sc}" | awk '{print $1}')
+  PROVISIONER=$(echo "${sc}" | awk '{print $2}')
 
-  if [[ "$PROVISIONER" =~ .*"csi".* ]]; then
-    echo "Updating storage class $SC_NAME as the default ..."
-    kubectl annotate sc/$SC_NAME storageclass.kubernetes.io/is-default-class="true"
+  if [[ "${PROVISIONER}" =~ .*"csi".* ]]; then
+    echo "Updating storage class ${SC_NAME} as the default ..."
+    kubectl annotate sc/"${SC_NAME}" storageclass.kubernetes.io/is-default-class="true"
 
     exit 0
   fi
