e1ffefc4c8b57b47e7553c83de1807ae8e33de30
John Strunk <jstrunk@redhat.com>
2023-10-25 13:55:30 -0400

    Enable TCP-level keepalive for rsync-tls mover
    Enabling TCP keepalives will hopefully allow VolSync to detect
    disconnections quicker in the presence of low-quality network
    connections
    
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- mover-rsync-tls/server.sh
+++ mover-rsync-tls/server.sh
@@ -77,6 +77,10 @@ debug = debug
 foreground = no
 output = /dev/stdout
 pid = $STUNNEL_PID_FILE
+socket = l:SO_KEEPALIVE=1
+socket = l:TCP_KEEPIDLE=180
+socket = r:SO_KEEPALIVE=1
+socket = r:TCP_KEEPIDLE=180
 syslog = no
 
 [rsync]
@@ -118,6 +122,10 @@ debug = debug
 foreground = no
 output = /dev/stdout
 pid = $STUNNEL_PID_FILE
+socket = l:SO_KEEPALIVE=1
+socket = l:TCP_KEEPIDLE=180
+socket = r:SO_KEEPALIVE=1
+socket = r:TCP_KEEPIDLE=180
 syslog = no
 
 [diskrsync]
