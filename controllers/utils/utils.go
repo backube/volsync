df76545f263478e3fc5a69091ae92b35fbd7b0d0
bo0tzz <git@bo0tzz.me>
2024-07-18 12:15:17 +0200

    feat: Set pod affinity from MoverConfig
    Signed-off-by: bo0tzz <git@bo0tzz.me>

--- controllers/utils/utils.go
+++ controllers/utils/utils.go
@@ -233,6 +233,9 @@ func UpdatePodTemplateSpecFromMoverConfig(podTemplateSpec *corev1.PodTemplateSpe
 	// Security context (nil by default)
 	podTemplateSpec.Spec.SecurityContext = moverConfig.MoverSecurityContext
 
+	// Affinity
+	podTemplateSpec.Spec.Affinity = moverConfig.MoverAffinity
+
 	// Adjust the job/deploy containers resourceRequirements based on resourceRequirements from the moverConfig
 	moverResources := defaultMoverResources
 	if moverConfig.MoverResources != nil {
