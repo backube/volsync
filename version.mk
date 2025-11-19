41590c30577486f8b5eb80e417956e3c2ec52753
Tesshu Flower <tflower@redhat.com>
2025-11-19 08:40:41 -0500

    Version for next release (v0.15.0)
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

--- version.mk
+++ version.mk
@@ -9,11 +9,11 @@
 #
 # Bundle Version being built right now and channels to use
 #
-VERSION := 0.14.0
+VERSION := 0.15.0
 # REPLACES_VERSION should be left empty for the first version in a new channel (See more info in Procedures.md)
 REPLACES_VERSION :=
 OLM_SKIPRANGE := '>=0.4.0 <$(VERSION)'
-CHANNELS := stable,stable-0.14
+CHANNELS := stable,stable-0.15
 DEFAULT_CHANNEL := stable
 MIN_KUBE_VERSION := 1.20.0
 
